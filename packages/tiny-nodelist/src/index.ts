0f79c798275ff3d312d6abfc92c48193ab4f4e36
Segun Adebayo <joseshegs@gmail.com>
2021-09-27 14:49:29 +0400

    chore: update nodelist

--- packages/tiny-nodelist/src/index.ts
+++ packages/tiny-nodelist/src/index.ts
@@ -13,13 +13,13 @@ export function indexOfId<T extends HTMLElement>(v: T[], id: string) {
   return item ? v.indexOf(item) : -1
 }
 
-export function nextById<T extends HTMLElement>(v: T[], id: string, loop?: boolean) {
+export function nextById<T extends HTMLElement>(v: T[], id: string, loop = true) {
   let idx = indexOfId(v, id)
   idx = loop ? (idx + 1) % v.length : Math.min(idx + 1, v.length - 1)
   return v[idx]
 }
 
-export function prevById<T extends HTMLElement>(v: T[], id: string, loop?: boolean) {
+export function prevById<T extends HTMLElement>(v: T[], id: string, loop = true) {
   let idx = indexOfId(v, id)
   idx = loop ? (idx - 1 + v.length) % v.length : Math.max(0, idx - 1)
   return v[idx]
