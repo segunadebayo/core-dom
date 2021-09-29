44cc93b6e69db828254c3bf63861269f5146d6cf
Segun Adebayo <joseshegs@gmail.com>
2021-09-30 01:06:29 +0400

    chore: update

--- packages/tiny-dom-query/src/is.ts
+++ packages/tiny-dom-query/src/is.ts
@@ -1,4 +1,4 @@
-const t = (v: any) => Object.prototype.toString.call(v).slice(8, -1)
+const isObj = (v: unknown): v is Record<string, any> => !(v == null || typeof v !== "object" || Array.isArray(v))
 
 export const isHTMLElement = (v: any): v is HTMLElement =>
-  v != null && /(HTML|SVG)\w+Element/.test(t(v)) && v.nodeType === Node.ELEMENT_NODE
+  isObj(v) && v.nodeType === Node.ELEMENT_NODE && typeof v.nodeName === "string"
